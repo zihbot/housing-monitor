2874ea5e9ae91bb334a2029ae3fb9cee0f0680ca
Botond Zih <zihbot@gmail.com>
2021-02-07 19:46:25 +0100

    Create web ui

---
+++ WebUi/housing-monitor/src/App.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import { HouseData } from './HouseData';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <HouseData />
+      </header>
+    </div>
+  );
+}
+
+export default App;
