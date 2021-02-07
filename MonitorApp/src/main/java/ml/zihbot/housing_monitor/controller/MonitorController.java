e82f464aa2bf843d196bf58d66246745599c17e5
Botond Zih <zihbot@gmail.com>
2021-02-07 19:44:08 +0100

    Update routing

--- MonitorApp/src/main/java/ml/zihbot/housing_monitor/controller/MonitorController.java
+++ MonitorApp/src/main/java/ml/zihbot/housing_monitor/controller/MonitorController.java
@@ -4,12 +4,14 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import ml.zihbot.housing_monitor.data_loader.DataLoaderClient;
 import ml.zihbot.housing_monitor.dto.KeyValuePair;
 
 @RestController
+@RequestMapping("rest")
 public class MonitorController {
     @Autowired
     DataLoaderClient dataLoaderClient;
