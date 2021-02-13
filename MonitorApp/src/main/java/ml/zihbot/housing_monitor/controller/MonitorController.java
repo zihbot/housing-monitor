a2495398f7ed7d682a497eb8ff6c05c62d2ad221
Botond Zih <zihbot@gmail.com>
2021-02-13 16:27:37 +0100

    Connect to Database

--- MonitorApp/src/main/java/ml/zihbot/housing_monitor/controller/MonitorController.java
+++ MonitorApp/src/main/java/ml/zihbot/housing_monitor/controller/MonitorController.java
@@ -8,9 +8,10 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import ml.zihbot.housing_monitor.config.DatabaseConfig;
 import ml.zihbot.housing_monitor.data_loader.DataLoaderClient;
 import ml.zihbot.housing_monitor.dto.KeyValuePair;
+import ml.zihbot.housing_monitor.entity.House;
+import ml.zihbot.housing_monitor.repository.HouseRepository;
 
 @RestController
 @RequestMapping("rest")
@@ -19,11 +20,14 @@ public class MonitorController {
     DataLoaderClient dataLoaderClient;
 
     @Autowired
-    DatabaseConfig databaseConfig;
+    HouseRepository houseRepository;
 
     @GetMapping("pairs")
     public List<KeyValuePair> getPairs() {
-        System.out.println(databaseConfig.getPassword());
-        return  dataLoaderClient.getPairs("https://ingatlan.com/ix-ker/elado+lakas/tegla-epitesu-lakas/31119133");
+        String url = "https://ingatlan.com/ix-ker/elado+lakas/tegla-epitesu-lakas/31119133";
+        List<House> houses = houseRepository.findByUrl(url);
+        House house = houses.isEmpty() ? new House(url) : houses.get(0);
+        houseRepository.save(house);
+        return  dataLoaderClient.getPairs(url);
     }
 }
