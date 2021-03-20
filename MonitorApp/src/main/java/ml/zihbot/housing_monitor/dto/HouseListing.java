ff62d377b6e0e283c837eb8f04bee48b38d0dbea
Botond Zih <zihbot@gmail.com>
2021-03-20 14:16:33 +0100

    Lombok

--- MonitorApp/src/main/java/ml/zihbot/housing_monitor/dto/HouseListing.java
+++ MonitorApp/src/main/java/ml/zihbot/housing_monitor/dto/HouseListing.java
@@ -1,5 +1,10 @@
 package ml.zihbot.housing_monitor.dto;
 
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
 public class HouseListing {
     private Long id;
     private String url;
@@ -9,22 +14,6 @@ public class HouseListing {
 
     public HouseListing(Long id, String url) {
         this.id = id;
-        this.url = url;
-    }
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getUrl() {
-        return url;
-    }
-
-    public void setUrl(String url) {
         this.url = url;
     }
 }
