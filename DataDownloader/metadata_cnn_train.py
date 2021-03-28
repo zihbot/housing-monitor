8c9c308419d6b564a86951ee94d31a2ca7d9825a
Botond Zih <zihbot@gmail.com>
2021-03-28 12:20:43 +0200

    trained model

--- DataDownloader/metadata_cnn_train.py
+++ DataDownloader/metadata_cnn_train.py
@@ -9,6 +9,8 @@ import tensorflow as tf
 size = 640, 480
 in_folder = '.img/'
 out_folder = '.img/resized/'
+#%%
+# Preprocessing
 # %%
 for filename in os.listdir(in_folder):
     try:
@@ -21,4 +23,78 @@ for filename in os.listdir(in_folder):
         im.save(out_folder + filename)
     except:
         print('Cannot resize image: ' + filename)
+
+#%%
+# Loading
+# %%
+batch_size=10
+seed = 42
+
+train_ds = tf.keras.preprocessing.image_dataset_from_directory(
+    out_folder,
+    validation_split=0.2,
+    subset="training",
+    seed=seed,
+    image_size=size,
+    batch_size=batch_size
+)
+test_ds = tf.keras.preprocessing.image_dataset_from_directory(
+    out_folder,
+    validation_split=0.2,
+    subset="validation",
+    seed=seed,
+    image_size=size,
+    batch_size=batch_size
+)
+# %%
+# Model
+#%%
+for image_batch, labels_batch in train_ds:
+  print(image_batch.shape)
+  print(labels_batch.shape)
+  break
+# %%
+from tensorflow.keras import layers
+num_classes = len(train_ds.class_names)
+
+model = tf.keras.Sequential([
+    layers.experimental.preprocessing.Rescaling(1./255,  name='normalizer'),
+    layers.Conv2D(10, 3, activation='relu', name='conv1'),
+    layers.MaxPooling2D(name='pool1'),
+    layers.Conv2D(10, 3, activation='relu', name='conv2'),
+    layers.MaxPooling2D(name='pool2'),
+    layers.Flatten(name='flat'),
+    layers.Dense(128, activation='relu', name='dense'),
+    layers.Dense(num_classes)
+])
+model.compile(
+  optimizer='adam',
+  loss=tf.losses.SparseCategoricalCrossentropy(from_logits=True),
+  metrics=['accuracy'])
+# %%
+# Train
+# %%
+model.fit(
+  train_ds,
+  validation_data=test_ds,
+  epochs=5
+)
+# %%
+# Save
+# %%
+model.save('model.h5')
+# %%
+model2 = tf.keras.models.load_model('model.h5')
+# %%
+import numpy as np
+for image, label in test_ds:
+    for i in range(len(label)):
+        #print(image[i].shape)
+        true = label[i]
+        if test_ds.class_names[true] != 'layout':
+            continue
+        print(test_ds.class_names[true])
+        predicted = model2.predict(tf.expand_dims(image[i],0))
+        print(predicted)
+        print(test_ds.class_names[np.argmax(predicted)])
 # %%
