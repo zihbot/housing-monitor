698c2eab654877b4088abb732cb6b844c3c8cacb
Botond Zih <zihbot@gmail.com>
2021-03-31 17:57:34 +0200

    Delete house on UI

--- WebUi/src/HouseDataClient.tsx
+++ WebUi/src/HouseDataClient.tsx
@@ -10,8 +10,9 @@ export const useRestGetClient = <T extends unknown>(uri: string): GetClient<T> =
 
     const getRequest = useCallback(() => {
         fetch('rest/' + uri)
-            .then(r => r.json())
-            .then(r => setResult({ data: r }) );
+            .then(r => {if (!r.ok) throw "REST error"; return r.json()})
+            .then(r => setResult({ data: r }) )
+            .catch(reason => setResult({ data: undefined }));
     }, []);
 
     useEffect(getRequest, []);
@@ -37,10 +38,34 @@ export const useRestPostClient = <T extends unknown>(
             },
             body: JSON.stringify(postData)
         })
-            .then(r => r.json())
+            .then(r => {if (!r.ok) throw "REST error"; return r.json()})
             .then(r => setResult({ data: r }))
+            .catch(reason => setResult({ data: undefined }))
             .finally(finishCallback);
     }, []);
 
     return {result, postRequest};
+};
+
+type DeleteClient<T> = {
+    result: {data: T | undefined},
+    deleteRequest: () => void
+}
+
+export const useRestDeleteClient = <T extends unknown>(
+    uri: string, 
+    finishCallback: () => void = () => {}): DeleteClient<T> => {
+    const [result, setResult] = useState<{data: T | undefined}>({ data: undefined });
+
+    const deleteRequest = useCallback(() => {
+        fetch('rest/' + uri, {
+            method: 'DELETE'
+        })
+            .then(r => {if (!r.ok) throw "REST error"; return r.json()})
+            .then(r => setResult({ data: r }) )
+            .catch(reason => setResult({ data: undefined }))
+            .finally(finishCallback);
+    }, []);
+
+    return {result, deleteRequest};
 };
